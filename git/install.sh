81def1cc326f65b2ad941543b0bf6fc77b24c751
stumash <stuart.mashaal@gmail.com>
2019-05-12 18:43:26 -0400

    it's working

--- git/install.sh
+++ git/install.sh
@@ -3,5 +3,14 @@
 THIS_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 cd "${THIS_DIR}"
 
-[ -f "${HOME}/.gitconfig" ] && rm "${HOME}/.gitconfig"
-ln "$(readlink -f "_gitconfig")" "${HOME}/.gitconfig"
+[[ ! -f "${HOME}/.gitconfig" ]] && cp "$(readlink -f "_gitconfig")" "${HOME}/.gitconfig"
+
+FOUND_GIT_ALIASES=
+FOUND_GIT_PUSH=
+while read LINE; do
+    [[ "${LINE}" == "[alias]" ]] && FOUND_GIT_ALIASES="true"
+    [[ "${LINE}" == "[push]" ]]  && FOUND_GIT_PUSH="true"
+done < "${HOME}/.gitconfig"
+if [[ -z "${FOUND_GIT_ALIASES}" && -z "${FOUND_GIT_PUSH}" ]]; then
+    cat "$(readlink -f "_gitconfig")" >> "${HOME}/.gitconfig"
+fi
--- other/install.sh
+++ other/install.sh
@@ -4,14 +4,16 @@ THIS_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 cd "${THIS_DIR}"
 
 FILEMAP=(
-    ["_haskeline"] "${HOME}/.haskeline"
-    ["_latexmkrc"] "${HOME}/.latexmkrc"
-    ["_tmux.conf"] "${HOME}/.tmux.conf"
+    ["_haskeline"]="${HOME}/.haskeline"
+    ["_latexmkrc"]="${HOME}/.latexmkrc"
+    ["_tmux.conf"]="${HOME}/.tmux.conf"
 )
 
 for TARGET in "${!FILEMAP[@]}"; do
     LINK="${FILEMAP[${TARGET}]}"
     TARGET="$(readlink -f "${TARGET}")"
+
+    echo "LINK: ${LINK} , TARGET: ${TARGET}"
 
     [ -f "${TARGET}" ] && rm "${TARGET}"
     ln "${TARGET}" "${LINK}"
