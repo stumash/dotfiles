7a284dd7866c10f095903bc6f873927fb698eb7f
stumash <stuart.mashaal@gmail.com>
2019-11-07 03:49:29 -0500

    add python path to _bash_env

--- bash/_bash_env.sh
+++ bash/_bash_env.sh
@@ -1,6 +1,6 @@
 # always use vim!
 EDITOR=nvim
-VISUAL=$EDITOR 
+VISUAL=$EDITOR
 export EDITOR VISUAL
 COLORTERM=gnome-terminal #nvim is still under development, so
 export COLORTERM # setting + exporting COLORTERM is needed for now
@@ -8,3 +8,7 @@ export COLORTERM # setting + exporting COLORTERM is needed for now
 # virtualenv shouldn't be messing with my PS1 variable (shell prompt)
 VIRTUAL_ENV_DISABLE_PROMPT=1
 export VIRTUAL_ENV_DISABLE_PROMPT
+
+# PATH
+PATH="$PATH:$(python3 -m site --user-base)/bin"
+export PATH
