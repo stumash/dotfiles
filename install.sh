015a8fe14c4fb3b0847e829a6b86630ba39e7561
stumash <stuart.mashaal@gmail.com>
2019-11-20 10:23:51 -0500

    add mkdir -p ~/installed_software/bin

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 THIS_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 cd "${THIS_DIR}"
 
+mkdir -p "$HOME/installed_software/bin"
 
 for DOTFILE_INSTALL_SCRIPT in "bash" "git" "vim" "other"; do
     ./"${DOTFILE_INSTALL_SCRIPT}"/install.sh
