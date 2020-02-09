591b60f0b3fbe241784c3636d203545c99696abf
stumash <stuart.mashaal@gmail.com>
2020-02-08 23:51:51 -0500

    fish/install.sh: 'apt install fish' -> 'which fish || apt install fish'

--- fish/install.sh
+++ fish/install.sh
@@ -3,6 +3,6 @@
 THIS_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 cd "${THIS_DIR}"
 
-sudo apt-get install fish
+which fish || sudo apt-get install fish
 
 "${THIS_DIR}/install.fish" "${@}"
