58380963145f386a78f58e27f60014e074c866f1
Stuart Mashaal <stuart.mashaal@gmail.com>
2025-10-08 15:36:22 -0400

    git alias fixes

--- zsh/.zsh_ssh.zsh
+++ zsh/.zsh_ssh.zsh
@@ -1,3 +1,1 @@
 eval $(keychain --eval --quiet ~/.ssh/flrsh_newlaptop_key2 ~/.ssh/stumash_newlaptop)
-zstyle :omz:plugins:ssh-agent lazy yes
-zstyle :omz:plugins:ssh-agent quiet yes
--- zsh/.zshrc_code.zsh
+++ zsh/.zshrc_code.zsh
@@ -2,7 +2,7 @@ ZVM_INIT_MODE=sourcing
 # oh-my-zsh
 export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME=""
-plugins=(git zsh-syntax-highlighting zsh-autosuggestions zsh-vi-mode ssh gradle asdf fzf ssh-agent)
+plugins=(git zsh-syntax-highlighting zsh-autosuggestions zsh-vi-mode gradle asdf fzf)
 source $ZSH/oh-my-zsh.sh
 
 # path
